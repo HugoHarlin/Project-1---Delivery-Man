25d4a3fee11d9ef424d94ae2d2f073a6c2cc6cbc
HugoHarlin <36413511+HugoHarlin@users.noreply.github.com>
2018-09-12 17:51:10 +0200

    d
    b

--- carmove.R
+++ carmove.R
@@ -16,7 +16,7 @@ carmove = function (roads, car, packages)
     toGo = car$load
     offset = 2
   }
-
+  
   # we create our starting node and add it to the frontier list
   start_node = list(x=0, y=0, cost=0, heur=0, path=list());
   start_node$x = car$x;
@@ -32,7 +32,7 @@ carmove = function (roads, car, packages)
   {
     expanded = frontier[[1]];
     frontier = frontier[-1];
-
+    
     
     # if expanded node is the target node, we are done!
     if(expanded$x == packages[toGo, 1 + offset] && expanded$y == packages[toGo, 2 + offset])
@@ -55,13 +55,28 @@ carmove = function (roads, car, packages)
         newNode$y = expanded$y;
         newNode$cost = expanded$cost + roads$hroads[expanded$x-1,expanded$y];
         newNode$heur = manhattanDist(expanded$x-1,expanded$y, packages[toGo, 1+offset],packages[toGo, 2+offset]);
-        newNode$path[[1]] = list(x = expanded$x-1, y = expanded$y);
+        newNode$path = expanded$path;
+        newNode$path[[length(newNode$path)+1]] = list(x = expanded$x-1, y = expanded$y);
         #show("new node")
         #show(newNode)
-        show(expanded$path)
+        #show(expanded$path)
+        
         # We add the new node to the frontier, sorted with respect to the total cost of the path
-        frontier = appendSorted(newNode,frontier);
+        flag2 = 1;
+        length = length(expanded$path);
+        if(length>0){
+          for (i in 1:length(expanded$path)){
+            if(expanded$path[[i]]$x == newNode$x && expanded$path[[i]]$y == newNode$y)
+              flag2 = 0; 
+          }
+        }
+        # We add the new node to the frontier, sorted with respect to the total cost of the path
+        if(flag2){
+          frontier = appendSorted(newNode,frontier);
+        }
+        
       }
+      
       
       # checks if movement to the right is possible
       if(expanded$x < 10)
@@ -72,9 +87,21 @@ carmove = function (roads, car, packages)
         newNode$y = expanded$y;
         newNode$cost = expanded$cost + roads$hroads[expanded$x,expanded$y];
         newNode$heur = manhattanDist(expanded$x+1,expanded$y, packages[toGo, 1+offset],packages[toGo, 2+offset]);
-        newNode$path[[1]] = list(x = expanded$x+1, y = expanded$y);
+        newNode$path = expanded$path;
+        newNode$path[[length(newNode$path)+1]] = list(x = expanded$x+1, y = expanded$y);
+        
+        flag2 = 1;
+        length = length(expanded$path);
+        if(length>0){
+        for (i in 1:length(expanded$path)){
+          if(expanded$path[[i]]$x == newNode$x && expanded$path[[i]]$y == newNode$y)
+            flag2 = 0; 
+        }
+      }
         # We add the new node to the frontier, sorted with respect to the total cost of the path
-        frontier = appendSorted(newNode,frontier);
+        if(flag2){
+          frontier = appendSorted(newNode,frontier);
+        }
       }
       
       # checks if movement upp is possible
@@ -86,9 +113,23 @@ carmove = function (roads, car, packages)
         newNode$y = expanded$y+1;
         newNode$cost = expanded$cost + roads$vroads[expanded$x,expanded$y];
         newNode$heur = manhattanDist(expanded$x,expanded$y+1, packages[toGo, 1+offset],packages[toGo, 2+offset]);
-        newNode$path[[1]] = list(x = expanded$x, y = expanded$y+1);
+        newNode$path = expanded$path;
+        newNode$path[[length(newNode$path)+1]] = list(x = expanded$x, y = expanded$y+1);
+        
         # We add the new node to the frontier, sorted with respect to the total cost of the path
-        frontier = appendSorted(newNode,frontier);
+        flag2 = 1;
+        length = length(expanded$path);
+        if(length>0){
+          for (i in 1:length(expanded$path)){
+            if(expanded$path[[i]]$x == newNode$x && expanded$path[[i]]$y == newNode$y)
+              flag2 = 0; 
+          }
+        }
+        # We add the new node to the frontier, sorted with respect to the total cost of the path
+        if(flag2){
+          frontier = appendSorted(newNode,frontier);
+        }
+        
       }
       
       # checks if movement upp is down
@@ -99,17 +140,28 @@ carmove = function (roads, car, packages)
         newNode$y = expanded$y-1;
         newNode$cost = expanded$cost + roads$vroads[expanded$x,expanded$y-1]; 
         newNode$heur = manhattanDist(expanded$x,expanded$y-1, packages[toGo, 1+offset],packages[toGo, 2+offset]);
-        newNode$path[[1]] = list(x = expanded$x, y = expanded$y-1);   
+        newNode$path = expanded$path;
+        newNode$path[[length(newNode$path)+1]] = list(x = expanded$x, y = expanded$y-1);   
         # We add the new node to the frontier, sorted with respect to the total cost of the path
-        frontier = appendSorted(newNode,frontier);
+        
+        flag2 = 1;
+        length = length(expanded$path);
+        if(length>0){
+          for (i in 1:length(expanded$path)){
+            if(expanded$path[[i]]$x == newNode$x && expanded$path[[i]]$y == newNode$y)
+              flag2 = 0; 
+          }
+        }
+        # We add the new node to the frontier, sorted with respect to the total cost of the path
+        if(flag2){
+          frontier = appendSorted(newNode,frontier);
+        }
+        
       }
       
-      # We add the new node to the frontier, sorted with respect to the total cost of the path
-      #frontier = appendSorted(newNode,frontier);
- 
       
     } 
-      
+    
   }   
   #show(expanded)
   if(length(expanded[[1]]) == 0){
